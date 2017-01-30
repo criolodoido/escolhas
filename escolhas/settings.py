28fb9424ad30b48dcfb411e0611263a4b93b0e1f
Victor Santos Silva <victor.santos.cd@gmail.com>
2017-01-30 01:00:53 -0200

    Update settings.py

--- escolhas/settings.py
+++ escolhas/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'escolhas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'banco',
+        'USER': 'visus',
+        'PASSWORD': 'sentinela',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
