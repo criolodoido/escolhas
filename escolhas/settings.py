9f79ed22a81042ac802425005c5bfa3a0afd1239
Victor Santos Silva <victor.santos.cd@gmail.com>
2017-01-31 10:38:39 -0200

    Update settings.py

--- escolhas/settings.py
+++ escolhas/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os, dj-database-url
+import os
+import dj-database-url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
